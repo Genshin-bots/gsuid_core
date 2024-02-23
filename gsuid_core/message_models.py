7797e581a000b7589efc121735d03047c0ec4e12
季落 <34079036+jiluoQAQ@users.noreply.github.com>
2024-02-23 11:23:00 +0800

    ✨ 添加支持QQ的点击发送按钮类型

--- gsuid_core/message_models.py
+++ gsuid_core/message_models.py
@@ -8,9 +8,9 @@ class Button(Struct):
     data: str  # 具体数据
     pressed_text: Optional[str] = None  # 按下之后显示的值
     style: Literal[0, 1] = 1  # 0灰色线框，1蓝色线框
-    action: Literal[-1, 0, 1, 2] = (
+    action: Literal[-1, 0, 1, 2, 3] = (
         -1
-    )  # 0跳转按钮，1回调按钮，2命令按钮, 【-1自适应】
+    )  # 0跳转按钮，1回调按钮，2命令按钮，3点击发送按钮, 【-1自适应】
     permisson: Literal[0, 1, 2, 3] = (
         2  # 0指定用户，1管理者，2所有人可按，3指定身份组
     )
