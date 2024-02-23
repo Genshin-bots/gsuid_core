c8c9fc43f8c3ba0a571e1732d2dae6568d6680f7
qianxu <qianxuuuu@qq.com>
2024-02-23 16:04:13 +0800

    🚀 更新资源站

--- gsuid_core/utils/download_resource/download_core.py
+++ gsuid_core/utils/download_resource/download_core.py
@@ -69,9 +69,9 @@ async def check_speed():
     URL_LIB = {
         '[JPFRP]': 'http://jp-2.lcf.icu:13643',
         '[HKFRP]': 'http://hk-1.5gbps-2.lcf.icu:10200',
-        '[Chuncheon]': 'https://kr.qxxx.tech',
-        '[Seoul]': 'https://kr-s.qxxx.tech',
-        '[Tokyo]': 'https://jp.qxqx.me',
+        '[Chuncheon]': 'https://kr.qxqx.cf',
+        '[Seoul]': 'https://kr-s.qxqx.cf',
+        '[Singapore]': 'https://sg.qxqx.cf',
     }
 
     TAG, BASE_URL = await find_fastest_url(URL_LIB)
