261f895eed67a51a6d6f7644a483e7511354c68b
KimigaiiWuyi <444835641@qq.com>
2025-07-07 05:44:58 +0800

    🎨 优化`_mys_request`

--- gsuid_core/utils/api/mys/base_request.py
+++ gsuid_core/utils/api/mys/base_request.py
@@ -4,11 +4,13 @@ import copy
 import time
 import uuid
 import random
+import asyncio
 from string import digits
 from abc import abstractmethod
 from typing import Any, Dict, Tuple, Union, Literal, Optional, overload
 
 import aiohttp
+from async_timeout import timeout
 
 from gsuid_core.bot import call_bot
 from gsuid_core.logger import logger
@@ -412,6 +414,10 @@ class BaseMysApi:
             app_cookie = await self.get_stoken(uid)
             if app_cookie is None:
                 return logger.warning('设备登录流程错误...')
+
+        if fp is None:
+            fp = await self.generate_fake_fp(device_id, seed_id, seed_time)
+
         await self.device_login_and_save(
             device_id, fp, device_info, app_cookie
         )
@@ -431,6 +437,7 @@ class BaseMysApi:
         use_proxy: Optional[bool] = False,
         base_url: Optional[str] = None,
         game_name: Optional[str] = None,
+        time_out: Optional[int] = 300,
     ) -> Union[Dict, int]:
         if params:
             params = {
@@ -467,28 +474,36 @@ class BaseMysApi:
                 uid = params['uid']
 
             if uid is not None:
-                device_id = await self.get_user_device_id(
-                    uid,
-                    game_name,
-                )
-                header['x-rpc-device_fp'] = await self.get_user_fp(
-                    uid,
-                    game_name,
-                )
-                if device_id is not None:
-                    header['x-rpc-device_id'] = device_id
+                try:
+                    async with timeout(5):
+                        device_id = await self.get_user_device_id(
+                            uid,
+                            game_name,
+                        )
+                        header['x-rpc-device_fp'] = await self.get_user_fp(
+                            uid,
+                            game_name,
+                        )
+                        if device_id is not None:
+                            header['x-rpc-device_id'] = device_id
 
-                dfp: Optional[str] = await GsUser.get_user_attr_by_uid(
-                    uid, 'device_info', 'sr' if self.is_sr else game_name
-                )
-                if dfp is not None:
-                    df = dfp.split('/')
-                    header['User-Agent'] = (
-                        f"Mozilla/5.0 (Linux; Android 13; {df[1]} {df[3]} "
-                        "; wv)AppleWebKit/537.36 (KHTML, like Gecko) "
-                        "Version/4.0 Chrome/104.0.5112.97"
-                        f"Mobile Safari/537.36 miHoYoBBS/2{mys_version}"
-                    )
+                        dfp: Optional[str] = await GsUser.get_user_attr_by_uid(
+                            uid,
+                            'device_info',
+                            'sr' if self.is_sr else game_name,
+                        )
+                        if dfp is not None:
+                            df = dfp.split('/')
+                            header['User-Agent'] = (
+                                "Mozilla/5.0 (Linux; Android 13; "
+                                f"{df[1]} {df[3]} "
+                                "; wv)AppleWebKit/537.36 (KHTML, like Gecko) "
+                                "Version/4.0 Chrome/104.0.5112.97"
+                                "Mobile Safari/537.36 miHoYoBBS/2"
+                                f"{mys_version}"
+                            )
+                except asyncio.TimeoutError:
+                    logger.warning('[mhy_request] 获取DFP超时, 未知原因...')
 
             logger.debug(header)
 
@@ -500,7 +515,7 @@ class BaseMysApi:
                         headers=header,
                         params=params,
                         json=data,
-                        timeout=aiohttp.ClientTimeout(total=300),
+                        timeout=aiohttp.ClientTimeout(total=time_out),
                         proxy=proxy,
                     ) as resp:
                         raw_data = await resp.json()
