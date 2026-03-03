48ca53db4cc42204d8420484d26a54c8abf094f7
KimigaiiWuyi <444835641@qq.com>
2026-03-04 04:49:46 +0800

    📝 补充一些函数文档

--- gsuid_core/ai_core/register.py
+++ gsuid_core/ai_core/register.py
@@ -14,7 +14,15 @@ _ALIASES: Dict[str, List[str]] = {}
 
 
 def ai_alias(name: str, alias: Union[str, List[str]]):
-    """为特定实体注册别名"""
+    """
+    为特定实体注册别名, 用于大模型调用前进行专有名词归一化
+    调用时, 例如:
+
+    from gsuid_core.ai_core.register import ai_alias
+
+    ai_alias("丝柯克", ['skk', '斯柯克'])
+
+    """
     if isinstance(alias, str):
         alias = [alias]
 
@@ -59,7 +67,31 @@ def ai_tools(
     可选参数:
         check_func: 一个异步函数，用于在执行工具前进行验证。
                     函数签名应为 async def check_func(bot, ev, **kwargs) -> bool
+                    bot: Bot,
+                    ev: Event,
+                    bot和ev为事件触发时的bot和事件对象，可以不作为传参
+                    如果作为传参, 可以不在kwargs中传递, 会自动执行依赖注入
         **check_kwargs: 传递给 check_func 的额外参数
+
+    装饰的函数:
+        需要包含docstring, 用于生成工具的描述
+        所有的传参需要进行详细的类型提示, 包括所有的参数, 以便大模型调用
+
+        例如:
+
+        from typing import Tuple, Optional, Annotated
+        from msgspec import Meta
+
+        @ai_tools(check_func=check_pm)
+        async def deduct_user_points(
+            target_user_id: Annotated[str, Meta(description="目标用户的唯一标识 ID")],
+            point_num: Annotated[int, Meta(description="要扣除的积分数量,必须大于 0")],
+            ev: Event,
+        ) -> str:
+        '''
+        扣除目标用户的积分
+        '''
+            pass
     """
 
     def decorator(func: F) -> F:
@@ -100,6 +132,33 @@ def ai_entity(entity: KnowledgePoint):
     """
     将实体注册为大模型实体。
     在启动时，自动将实体存入全局注册表。
+
+        entity: 一个包含实体信息的字典
+
+            id: str
+            plugin: str
+            type: str
+            category: str
+            title: str
+            content: str
+            tags: List[str]
+            _hash: str
+
+    例如:
+
+    from gsuid_core.ai_core.models import KnowledgePoint
+    from gsuid_core.ai_core.register import ai_entity
+
+    ai_entity(KnowledgePoint(
+        id="123",
+        plugin="Genshin",
+        type="角色介绍",
+        category="角色",
+        title="角色介绍和详情 - 丝柯克",
+        content="角色的详细信息, # 丝柯克 ## 武器类型xx ## 技能 ## 命之座",
+        tags=["角色", "丝柯克", "skk", "Genshin"],
+        _hash="123456",
+    ))
     """
     _ENTITIES.append(entity)
     logger.trace(f"🧠 [AI][Registry] Entity registered: {entity['title']}")
